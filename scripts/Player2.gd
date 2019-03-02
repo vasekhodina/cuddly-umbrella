d1de0f2ee4b9c9c82f20f3a16199519f560379fb
Vasek Escaron Hodina <wasseker@gmail.com>
2019-03-02 20:54:41 +0100

    Add first working prototype
    Prior to this change,
    
    This change

---
+++ scripts/Player2.gd
@@ -0,0 +1,26 @@
+extends KinematicBody2D
+
+export (int) var SPEED
+export (float) var ROTATION_SPEED
+var screensize
+var velocity = Vector2()
+
+
+func _ready():
+    # Called every time the node is added to the scene.
+    # Initialization here
+    screensize = get_viewport_rect().size
+
+func _process(delta):
+    velocity = Vector2()
+    var rotation_dir = 0
+    if Input.is_action_pressed("p2_right"):
+        rotation_dir += 1
+    if Input.is_action_pressed("p2_left"):
+        rotation_dir -= 1
+    if Input.is_action_pressed("p2_down"):
+        velocity = Vector2(-SPEED, 0).rotated(rotation)
+    if Input.is_action_pressed("p2_up"):
+        velocity = Vector2(SPEED, 0).rotated(rotation)
+    rotation += rotation_dir * ROTATION_SPEED * delta
+    move_and_slide(velocity)
