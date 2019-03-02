6cfac67826245aae1f6566938d635c7d0b24197e
unknown <toptygin@gmail.com>
2019-03-02 21:59:51 +0100

    P2 also shoots

--- scripts/Player2.gd
+++ scripts/Player2.gd
@@ -4,6 +4,9 @@ export (int) var SPEED
 export (float) var ROTATION_SPEED
 var screensize
 var velocity = Vector2()
+export (PackedScene) var bullet
+onready var bullet_container = get_node("bullet_container")
+onready var gun_timer = get_node("gun_timer")
 
 
 func _ready():
@@ -22,5 +25,14 @@ func _process(delta):
         velocity = Vector2(-SPEED, 0).rotated(rotation)
     if Input.is_action_pressed("p2_up"):
         velocity = Vector2(SPEED, 0).rotated(rotation)
+    if Input.is_action_pressed("p2_shoot") and gun_timer.get_time_left() == 0:
+        shoot()
     rotation += rotation_dir * ROTATION_SPEED * delta
     move_and_slide(velocity)
+	
+func shoot():
+    gun_timer.start()
+    var b1 = bullet.instance()
+    bullet_container.add_child(b1)
+    b1.start_at(rotation, get_node("muzzle").get_global_position(), velocity)
+
